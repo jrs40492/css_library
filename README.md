13ff3c7da69eba8d7998557f03c0bd989c157c03
Jacob Swanson <jrs40492@gmail.com>
2019-03-10 23:17:21 -0400

    Update readme. Some styling changes

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# CSS Framework
+# Stylish CSS Framework
+
+## Demo
+
+http://stylish.jacobrswanson.com (Not yet live)
+
+## Why?
+
+After using several different CSS frameworks, I realized that none of them truly work the way I want them to. Margins and padding were just not right or even worse, I'd implement version X and then version Y would come out, completly changing how the framework works. This, in turn, required tons of re-work to update to the new version.
+
+# TODO
+
+- Implement versioning/build numbers
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefix = require('gulp-autoprefixer');
 
 sass.compiler = require('node-sass');
 
 gulp.task('sass', () => {
   return gulp
     .src('./sass/main.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(
+      sass({
+        outputStyle: 'compressed'
+      }).on('error', sass.logError)
+    )
+    .pipe(
+      autoprefix({
+        browsers: ['> 1%', 'last 2 versions', 'Firefox ESR'],
+        cascade: false
+      })
+    )
     .pipe(gulp.dest('.'));
 });
 
